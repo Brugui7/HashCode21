9cefe35457a9942f8a1c00e6e3fc7310770f3dbe
Lagrang3 <eduardo.quintana@pm.me>
2021-02-23 19:45:37 +0100

    plots

---
+++ practice/pizza-hist.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from sys import stdin
+import sys
+
+
+hist =[]
+fname = sys.argv[1]
+outname= sys.argv[2]
+with open(fname,'r') as f:
+    for line in f:
+        nlist = line.split()
+        hist += map(int,nlist)
+
+#print(hist)
+
+fig = plt.figure()
+ax = fig.subplots(1,1)
+ax.bar(range(len(hist)),hist)
+ax.set_xlabel('size of pizza')
+ax.set_ylabel('frequency')
+plt.savefig(outname)
