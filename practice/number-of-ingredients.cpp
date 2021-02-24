e52a5dcf4508d9fede38d021d5ced666e3821199
Lagrang3 <eduardo.quintana@pm.me>
2021-02-24 09:17:06 +0100

    added also max size of pizza

--- practice/number-of-ingredients.cpp
+++ practice/number-of-ingredients.cpp
@@ -2,6 +2,7 @@
 #include <set>
 #include <string>
 #include <array>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -10,10 +11,12 @@ int main()
     int N;
     array<int,3> M;
     cin>>N;
+    int max_ing=-1;
     for(auto &m : M)cin>>m;
     for(int i=0;i<N;++i)
     {
         int n; cin>>n;
+        max_ing = max(max_ing,n);
         for(int j=0;j<n;++j)
         {
             string s;cin>>s;
@@ -22,6 +25,7 @@ int main()
     }
     cout << "#pizzas = " << N << '\n';
     cout << "#ingredients = " << I.size() << '\n';
+    cout << "max ingredients = " << max_ing << '\n';
     
     return 0;
 }
