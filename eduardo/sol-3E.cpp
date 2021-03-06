ac49ea931e762a68f6d78a80354d437ca8389fc8
Lagrang3 <eduardo.quintana@pm.me>
2021-03-06 08:26:56 +0100

    clang-format

--- eduardo/sol-3E.cpp
+++ eduardo/sol-3E.cpp
@@ -1,119 +1,117 @@
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 using namespace std;
 
 constexpr int FIXED_TIME = 100;
 
 struct intersection_t
 {
-    vector<int> in,out;
+    vector<int> in, out;
     int id;
 };
 
 struct street_t
 {
-    int beg,end,L;
+    int beg, end, L;
     string name;
     int count{};
 };
 
 int main()
 {
-    map<string,int> street_name;
-    int D,I,S,V,F;
-    cin>>D>>I>>S>>V>>F;
-    
+    map<string, int> street_name;
+    int D, I, S, V, F;
+    cin >> D >> I >> S >> V >> F;
+
     vector<int> distance(I);
     vector<intersection_t> intersection(I);
     vector<street_t> street(S);
-    
-    for(int i=0;i<I;++i)
+
+    for (int i = 0; i < I; ++i)
         intersection[i].id = i;
-    
-    for(int i=0;i<S;++i)
+
+    for (int i = 0; i < S; ++i)
     {
-        auto &s = street[i];
-        cin>>s.beg>>s.end>>s.name>>s.L;
-        
+        auto& s = street[i];
+        cin >> s.beg >> s.end >> s.name >> s.L;
+
         street_name[s.name] = i;
         intersection[s.beg].out.push_back(i);
         intersection[s.end].in.push_back(i);
-        
-        if(s.beg!=499 and s.end!=499)
-            distance[s.end]=s.L;
+
+        if (s.beg != 499 and s.end != 499)
+            distance[s.end] = s.L;
     }
-    
-    for(int i=0;i<V;++i)
+
+    for (int i = 0; i < V; ++i)
     {
-        int n;cin>>n;
+        int n;
+        cin >> n;
         string name;
-        for(int j=0;j<n;++j)
+        for (int j = 0; j < n; ++j)
         {
-            cin>>name;
+            cin >> name;
             int s = street_name[name];
-            street[s].count ++;
+            street[s].count++;
         }
     }
-    
+
     // solution
-    
-    for(int i=0;i<intersection.size();)
+
+    for (int i = 0; i < intersection.size();)
     {
-        auto & in_vec = intersection[i].in;
-        for(int j=0;j<in_vec.size();)
+        auto& in_vec = intersection[i].in;
+        for (int j = 0; j < in_vec.size();)
         {
-            int s_id = in_vec[j]; 
-            if(street[s_id].count)
+            int s_id = in_vec[j];
+            if (street[s_id].count)
                 ++j;
             else
             {
-                swap( in_vec[j],in_vec.back());
+                swap(in_vec[j], in_vec.back());
                 in_vec.pop_back();
             }
         }
-        
-        if(in_vec.size())
+
+        if (in_vec.size())
             ++i;
         else
         {
-            swap(intersection[i],intersection.back());
+            swap(intersection[i], intersection.back());
             intersection.pop_back();
         }
     }
-    
-    
+
     cout << intersection.size() << '\n';
-    for(auto &i : intersection)
+    for (auto& i : intersection)
     {
         cout << i.id << '\n';
-        auto &in_vec = i.in;
+        auto& in_vec = i.in;
         cout << in_vec.size() << '\n';
-        
-        if(i.id==499)
+
+        if (i.id == 499)
         {
-            for(auto j : in_vec)
+            for (auto j : in_vec)
             {
                 cout << street[j].name << ' ' << street[j].count << '\n';
             }
-        
-        }else
-        {   
+        }
+        else
+        {
             int T = distance[i.id];
-            for(auto j : in_vec)
+            for (auto j : in_vec)
             {
                 int val = 1;
-                
-                if(street[j].beg != 499 and street[j].end != 499)
-                    val = T-1;
-                
-                cout << street[j].name << ' ' << min(D,max(1,val)) << '\n';
+
+                if (street[j].beg != 499 and street[j].end != 499)
+                    val = T - 1;
+
+                cout << street[j].name << ' ' << min(D, max(1, val)) << '\n';
             }
-        
         }
-        
     }
-    
+
     return 0;
 }
--- eduardo/sol-simulation.cpp
+++ eduardo/sol-simulation.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-#include <fstream>
-#include <queue>
-#include <vector>
-#include <set>
-#include <map>
 #include <algorithm>
-#include <limits>
 #include <cassert>
+#include <fstream>
+#include <iostream>
+#include <limits>
 #include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
 
 // predicted scores
 // A: 1'001
@@ -27,69 +27,61 @@
 
 class name_resolution
 {
-    std::map<std::string,int> id;
+    std::map<std::string, int> id;
     std::vector<std::string> name;
-    
-    public:
-    int get_id(const std::string& a)const
-    {
-        return id.at(a);
-    }
-    auto get_name(int i)const
-    {
-        return name.at(i);
-    }
-    void add(int i,std::string a)
+
+   public:
+    int get_id(const std::string& a) const { return id.at(a); }
+    auto get_name(int i) const { return name.at(i); }
+    void add(int i, std::string a)
     {
         name.push_back(a);
-        id[a]=i;
+        id[a] = i;
     }
 };
-
-struct street_t;
 
 struct car_t
 {
-    //
+    // a car as given in the input
     const name_resolution& strdb;
     int id;
-    int pos{0};
     int time_to_finish{0};
     std::vector<int> path;
-    car_t(const name_resolution& str_name):
-        strdb{str_name}
-    {}
-    void add_path(int st,int L)
+    car_t(int car_id, const name_resolution& str_name)
+        : id{car_id}, strdb{str_name}
+    {
+    }
+    void add_path(int st, int L)
     {
         path.push_back(st);
-        if(path.size()>1)
+        if (path.size() > 1)
             time_to_finish += L;
     }
-    int get_street()const
-    {
-        return path.at(pos);
-    }
-    void advance()
-    {
-        ++pos;
-    }
-    bool finish()const
-    {
-        return pos==(path.size()-1);
-    }
-    static int id_counter;
-    
 };
-    std::ostream& operator << (std::ostream& os, const car_t& car)
+
+struct sim_car_t : car_t
+{
+    // simulation state
+    int pos{0};
+
+    sim_car_t(const car_t& c) : car_t{c} {}
+
+    void reset() { pos = 0; }
+    int get_street() const { return path.at(pos); }
+    void advance() { ++pos; }
+    bool finish() const { return pos == (path.size() - 1); }
+};
+
+std::ostream& operator<<(std::ostream& os, const car_t& car)
+{
+    os << "car id: " << car.id << '\n';
+    os << "path items: " << car.path.size() << '\n';
+    for (auto st : car.path)
     {
-        os << "car id: " << car.id << '\n';
-        os << "path items: " << car.path.size() << '\n';
-        for(auto st : car.path)
-        {
-            os << car.strdb.get_name(st) << ' ';
-        }
-        return os ;
+        os << car.strdb.get_name(st) << ' ';
     }
+    return os;
+}
 
 struct street_t
 {
@@ -98,79 +90,72 @@ struct street_t
     std::string name;
     int beg, end, L;
     int ncars{};
-    
-    void set_car(){ncars++;}
-    int get_cars()const {return ncars;}
-    
-    std::queue< std::pair<int,int> > Q;
-    
-    bool operator < (const street_t& that)const
-    {
-        return name < that.name;
-    }
+
+    void set_car() { ncars++; }
+    int get_cars() const { return ncars; }
+
+    std::queue<std::pair<int, int>> Q;
+
+    bool operator<(const street_t& that) const { return name < that.name; }
     static int id_counter;
 };
 
-int street_t::id_counter =0;
-int car_t::id_counter =0;
+int car_t::id_counter = 0;
 
 struct node_t
 {
     const name_resolution& strdb;
     int id;
-    node_t(int i,const name_resolution& str_name):
-        id{i},
-        strdb{str_name}
-    {}
-    std::vector<int> in,out;
-    std::vector< std::pair<int,int> > schedule;
+    node_t(int i, const name_resolution& str_name) : id{i}, strdb{str_name} {}
+    std::vector<int> in, out;
+    std::vector<std::pair<int, int>> schedule;
     std::vector<int> in_order;
     std::vector<bool> used;
-    std::map<int,int> inv_in; 
+    std::map<int, int> inv_in;
     int T{};
-    
+
     void build_schedule()
     {
         int last_free = 0;
-        for(int& i: in_order)
-        if(i<0)
-        {
-            while(used.at(last_free))
-                last_free++;
-            i = in.at(last_free);
-            used.at(last_free) = true;
-        }
-        
-        for(int i : in_order)
-            schedule.push_back({i,1});
+        for (int& i : in_order)
+            if (i < 0)
+            {
+                while (used.at(last_free))
+                    last_free++;
+                i = in.at(last_free);
+                used.at(last_free) = true;
+            }
+
+        for (int i : in_order)
+            schedule.push_back({i, 1});
     }
-    
+
     void set_preference(int time, int st)
     {
         time %= T;
         int pos = inv_in.at(st);
-        if(used.at(pos))return;
-        
+        if (used.at(pos))
+            return;
+
         used.at(pos) = true;
-        for(int t=0;t<T;++t)
+        for (int t = 0; t < T; ++t)
         {
-            time = (time+1)%T;
-            if(in_order.at(time)<0)
+            time = (time + 1) % T;
+            if (in_order.at(time) < 0)
             {
                 in_order.at(time) = st;
                 break;
             }
         }
     }
-    
-    bool active()const{return T>0;}
-    
-    int green(int time)const // returns street id
+
+    bool active() const { return T > 0; }
+
+    int green(int time) const  // returns street id
     {
         // TODO: an efficient way to get the light
-        if(T==0) 
-            throw std::runtime_error(
-            "node_t::green(): T=0");
+        if (T == 0)
+            throw std::runtime_error("node_t::green(): T=0");
         return in_order.at(time % T);
     }
     void init_schedule()
@@ -178,58 +163,58 @@ struct node_t
         T = in.size();
         in_order.resize(T);
         used.resize(T);
-        std::fill(in_order.begin(),in_order.end(),-1);
-        std::fill(used.begin(),used.end(),false);
-        
-        for(int i=0;i<in.size();++i)
+        std::fill(in_order.begin(), in_order.end(), -1);
+        std::fill(used.begin(), used.end(), false);
+
+        for (int i = 0; i < in.size(); ++i)
         {
-            inv_in[ in[i]  ] = i;
+            inv_in[in[i]] = i;
         }
     }
 };
 
-std::ostream& operator << (std::ostream& os, const node_t& node)
+std::ostream& operator<<(std::ostream& os, const node_t& node)
 {
     os << "node id: " << node.id << '\n';
-    os << "schedule:\n"; 
-    for(auto [i,t] : node.schedule)
+    os << "schedule:\n";
+    for (auto [i, t] : node.schedule)
     {
-        os <<  node.strdb.get_name(i)  <<" time=" <<t<<'\n';
+        os << node.strdb.get_name(i) << " time=" << t << '\n';
     }
     return os;
 }
 
-auto get_graph(int N_nodes, 
-    std::vector<street_t>& streets,
-    const name_resolution& str_name,
-    const std::vector<car_t>& cars,
-    const int T_max
-    )
+auto get_graph(int N_nodes,
+               std::vector<street_t>& streets,
+               const name_resolution& str_name,
+               const std::vector<car_t>& cars,
+               const int T_max)
 {
     std::vector<node_t> graph;
-    
-    for(int i=0;i<N_nodes;++i)
-        graph.emplace_back(i,str_name);
-    
-    for(auto& c: cars)
-    if(c.time_to_finish <= T_max){ // this car makes it
-        for(size_t i=0;i<c.path.size()-1;++i)
-        {
-            int id = c.path[i];
-            streets[id].set_car();
+
+    for (int i = 0; i < N_nodes; ++i)
+        graph.emplace_back(i, str_name);
+
+    for (auto& c : cars)
+        if (c.time_to_finish <= T_max)
+        {  // this car makes it
+            for (size_t i = 0; i < c.path.size() - 1; ++i)
+            {
+                int id = c.path[i];
+                streets[id].set_car();
+            }
         }
-    }
-    
+
     // construct graph
-    for(const auto& st : streets)
-    if(st.get_cars()) // use only important streets
-    {
-        graph[st.beg].out.push_back(st.id);
-        graph[st.end].in.push_back(st.id);
-    }
-    
+    for (const auto& st : streets)
+        if (st.get_cars())  // use only important streets
+        {
+            graph[st.beg].out.push_back(st.id);
+            graph[st.end].in.push_back(st.id);
+        }
+
     // TODO plan the lights: homogeneus assignment
-    for(auto &node : graph)
+    for (auto& node : graph)
     {
         node.init_schedule();
     }
@@ -241,107 +226,112 @@ struct simulation_t
     const int T_max, Bonus;
     int score{};
     int cars_arrived{};
-    int first=std::numeric_limits<int>::max();
+    int first = std::numeric_limits<int>::max();
     int last = -1;
-    int min_score=std::numeric_limits<int>::max();
-    int max_score=-1;
-    std::ostream &os;
-    std::set<int> busy_nodes; 
-    
-    simulation_t(int t_max,int bonus, 
-        std::vector<node_t>& graph,
-        std::vector<street_t>& streets,
-        std::vector<car_t>& cars,
-        std::ostream& o):
-        
-        T_max{t_max},Bonus{bonus},
-        os(o)
+    int min_score = std::numeric_limits<int>::max();
+    int max_score = -1;
+    std::ostream& os;
+    std::set<int> busy_nodes;
+
+    simulation_t(int t_max,
+                 int bonus,
+                 std::vector<node_t>& graph,
+                 std::vector<street_t>& streets,
+                 std::vector<car_t>& cars,
+                 std::ostream& o)
+        :
+
+          T_max{t_max},
+          Bonus{bonus},
+          os(o)
     {
-        for(int time=0;time<T_max;++time)
-        {   
-           // move cars at time steps, ignore empty intersections
-           busy_nodes.clear();
-           
-           for(const auto & car : cars)
-           if(not car.finish())
-           {
-                const auto &st = streets[car.get_street()];
-                busy_nodes.insert(st.end);
-           }
-           
-           
-           os << "Time: " << time << '\n';
-           for(auto no_id : busy_nodes)
-           {
-                auto & node = graph[no_id];
-                
-                if(node.green(time)<0)
+        for (int time = 0; time < T_max; ++time)
+        {
+            // move cars at time steps, ignore empty intersections
+            busy_nodes.clear();
+
+            for (const auto& car : cars)
+                if (not car.finish())
+                {
+                    const auto& st = streets[car.get_street()];
+                    busy_nodes.insert(st.end);
+                }
+
+            os << "Time: " << time << '\n';
+            for (auto no_id : busy_nodes)
+            {
+                auto& node = graph[no_id];
+
+                if (node.green(time) < 0)
                 {
                     // not decided yet
-                    for(int st_id : node.in)
+                    for (int st_id : node.in)
                     {
-                        auto &st = streets.at(st_id);
-                        if(st.Q.empty())continue;
+                        auto& st = streets.at(st_id);
+                        if (st.Q.empty())
+                            continue;
                         // got some non-empty queue
-                        
-                        auto [t,car_id] = st.Q.front();       
-                        if(t>time) continue;
+
+                        auto [t, car_id] = st.Q.front();
+                        if (t > time)
+                            continue;
                         // this car can move
-                        
-                        node.set_preference(time,st_id);
+
+                        node.set_preference(time, st_id);
                         break;
                     }
-                    
                 }
-                if(node.green(time)<0) continue;
-                
-                auto &st = streets .at( node.green(time) ); 
-                if(st.Q.empty())
+                if (node.green(time) < 0)
                     continue;
-                auto [t,car_id] = st.Q.front();       
-                auto &car = cars[car_id];
-                
+
+                auto& st = streets.at(node.green(time));
+                if (st.Q.empty())
+                    continue;
+                auto [t, car_id] = st.Q.front();
+                auto& car = cars[car_id];
+
                 os << "node id: " << node.id << '\n';
                 os << "green light on street: " << node.green(time) << '\n';
-                os << "size of queue " << st.Q.size() <<'\n';
-                os << "last element: time=" << t << " car=" << car.id <<'\n';
-                
-                if(t>time)
+                os << "size of queue " << st.Q.size() << '\n';
+                os << "last element: time=" << t << " car=" << car.id << '\n';
+
+                if (t > time)
                 {
                     continue;
-                }    
-                
+                }
+
                 st.Q.pop();
                 car.advance();
-                auto &new_st = streets [car.get_street() ];
-                
-                if(car.finish())
+                auto& new_st = streets[car.get_street()];
+
+                if (car.finish())
                 {
-                    os << "car " << car.id << " finished moving to end_list on street " 
-                        << new_st.id << " " << new_st.name <<'\n';
+                    os << "car " << car.id
+                       << " finished moving to end_list on street " << new_st.id
+                       << " " << new_st.name << '\n';
                     anotate_score(time + new_st.L);
                 }
                 else
                 {
-                    os << "car " << car.id <<" moving to queue on street " 
-                        << new_st.id <<'\n';
-                    new_st.Q.push({time+new_st.L,car.id});
+                    os << "car " << car.id << " moving to queue on street "
+                       << new_st.id << '\n';
+                    new_st.Q.push({time + new_st.L, car.id});
                 }
-                
-           }
+            }
         }
     }
     void anotate_score(int time)
     {
-        if(time>T_max)return;
+        if (time > T_max)
+            return;
         int pts = Bonus + T_max - time;
         score += pts;
         cars_arrived++;
-        first = std::min(first,time);
-        last = std::max(last,time);
-        min_score = std::min(min_score,pts);
-        max_score = std::max(max_score,pts);
-        os << "Anotate score=" << pts << " at time="<<time<<'\n';
+        first = std::min(first, time);
+        last = std::max(last, time);
+        min_score = std::min(min_score, pts);
+        max_score = std::max(max_score, pts);
+        os << "Anotate score=" << pts << " at time=" << time << '\n';
     }
 };
 
@@ -353,39 +343,40 @@ auto get_street(std::istream& is)
     return str;
 }
 auto get_car(std::istream& is,
-    const name_resolution& strdb,
-    const std::vector<street_t>& streets)
+             const name_resolution& strdb,
+             const std::vector<street_t>& streets)
 {
     car_t c{strdb};
-    int n; is>>n;
+    int n;
+    is >> n;
     c.id = c.id_counter++;
-    c.path.reserve(n);
-    while(n--)
+    while (n--)
     {
-        std::string name;is>>name;
-        int st =strdb.get_id(name);
-        c.add_path(st,streets[ st ].L);
+        std::string name;
+        is >> name;
+        int st = strdb.get_id(name);
+        c.add_path(st, streets[st].L);
     }
     return c;
 }
 
 void output_plan(std::ostream& os,
-    const std::vector<node_t>& graph,
-    const std::vector<street_t> &streets)
+                 const std::vector<node_t>& graph,
+                 const std::vector<street_t>& streets)
 {
-    int count=std::count_if(graph.begin(),graph.end(),
-        [](const node_t& no){ return no.active() ; } );
+    int count = std::count_if(graph.begin(), graph.end(),
+                              [](const node_t& no) { return no.active(); });
     os << count << '\n';
-    for(const auto &no : graph)
-    if(no.active())
-    {
-        os << no.id << '\n';
-        os << no.schedule.size() << '\n';
-        for(auto [s,t] : no.schedule )
+    for (const auto& no : graph)
+        if (no.active())
         {
-            os << streets[s].name << ' ' << t << '\n';
+            os << no.id << '\n';
+            os << no.schedule.size() << '\n';
+            for (auto [s, t] : no.schedule)
+            {
+                os << streets[s].name << ' ' << t << '\n';
+            }
         }
-    }
 }
 
 using namespace std;
@@ -394,31 +385,31 @@ int main()
     // read input data
     int T_max, N_nodes, N_streets, N_cars, Bonus;
     cin >> T_max >> N_nodes >> N_streets >> N_cars >> Bonus;
-    
+
     vector<street_t> streets;
     vector<car_t> cars;
     name_resolution strdb;
-    for(int i=0;i<N_streets;++i)
+    for (int i = 0; i < N_streets; ++i)
     {
         streets.push_back(get_street(cin));
-        strdb.add(streets.back().id,streets.back().name);
+        strdb.add(streets.back().id, streets.back().name);
     }
-    for(int i=0;i<N_cars;++i)
+    for (int i = 0; i < N_cars; ++i)
     {
-        car_t c = get_car(cin,strdb,streets);
+        car_t c = get_car(cin, strdb, streets);
         cars.push_back(c);
-        auto &st = streets[c.get_street()];
-        st.Q.push({0,i}); 
+        auto& st = streets[c.get_street()];
+        st.Q.push({0, i});
     }
-    // construct traffic light rules 
-    vector<node_t> city = get_graph(N_nodes,streets,strdb,cars,T_max);
-    
+    // construct traffic light rules
+    vector<node_t> city = get_graph(N_nodes, streets, strdb, cars, T_max);
+
     // run simulation
     ofstream log("/dev/null");
-    //ofstream log("log");
-    
-    simulation_t sim(T_max,Bonus,city,streets,cars,log);
-    
+    // ofstream log("log");
+
+    simulation_t sim(T_max, Bonus, city, streets, cars, log);
+
     // output scores
     cerr << "score: " << sim.score << '\n';
     cerr << "num cars arrivals: " << sim.cars_arrived << '\n';
@@ -426,10 +417,10 @@ int main()
     cerr << "max score: " << sim.max_score << '\n';
     cerr << "last arrival: " << sim.last << '\n';
     cerr << "min score: " << sim.min_score << '\n';
-    
-    for(auto &no : city)
+
+    for (auto& no : city)
         no.build_schedule();
-    
-    output_plan(cout,city,streets);
+
+    output_plan(cout, city, streets);
     return 0;
 }
